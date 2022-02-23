77a7dad1004f15366f1209679e5e0b20dd705ff6
Marih Souza <marina-queiroz2011@hotmail.com>
2022-02-23 12:18:15 -0300

    Que ano eu nasci a partir de dois inteiros

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+
+        var idade: Int = 0
+        var ano: Int = 0
+        var atual: Int = 2022
+
+        println("Digite sua idade: ")
+        idade = readLine()!!. toInt()
+
+        println("Digíte  seu ano atual: ")
+        ano = readLine()!!. toInt()
+
+        println("O ano do seu nascimento é ${atual-idade} ")
+
+
+
+    }
+
